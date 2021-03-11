eb9acd3184e2c12b724babc737c5b4f6d4eb6faf
Ginette Hartell <77505600+ghartell@users.noreply.github.com>
2021-03-11 12:09:59 -0500

    Creating new path for accuracy testing trials

---
+++ Accuracy Testing/demoMain.m
@@ -0,0 +1,130 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% BME70B: Biomedical Engineering Capstone Design
+% AT03 Robotic Automated Microscopy Marker Tracking and Robot Movement
+% Phase II Demonstration
+%
+% Author #1: Ginette Hartell - 500755250
+% Author #2: Mohammad Aziz Uddin - 500754765
+% Author #3: Jay Tailor - 500750496
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Initialize Peter Corke's Toolbox 
+petercorkeinitialize();
+
+%% Initialize Tracking System Parameters
+addpath(genpath('Trial 18-19'));
+load("stereoParams18.mat");
+
+pivotOffset = 200; % 20cm offset from midpoint btwn blue and green
+threshold = 240; % Threshold for Grayscale 
+
+readerLeft = VideoReader('myVideoLeftTrial18.avi');
+readerRight = VideoReader('myVideoRightTrial18.avi');
+
+%Set up for skipping n frames
+nFramesLeft = readerLeft.NumFrames;
+vidHeightLeft = readerLeft.Height;
+vidWidthLeft = readerLeft.Width;
+nFramesRight = readerRight.NumFrames; 
+vidHeightRight = readerRight.Height;
+vidWidthRight = readerRight.Width;
+
+mov(1:nFramesLeft) = ...
+    struct('readerLeft',zeros(vidHeightLeft,vidWidthLeft, 3,'uint8'),...
+           'readerRight',zeros(vidHeightRight,vidWidthRight, 3,'uint8'),...
+           'colormap',[]);
+
+for k = 1:nFramesLeft
+mov(k).readerLeft = read(readerLeft,k);
+mov(k).readerRight = read(readerRight,k);
+end
+
+% Set blob analysis handling
+hblob = vision.BlobAnalysis('AreaOutputPort', false, ... 
+                                'CentroidOutputPort', true, ... 
+                                'BoundingBoxOutputPort', true', ...
+                                'MinimumBlobArea', 1, ...
+                                'MaximumBlobArea', 20000, ...
+                                'MaximumCount',3);
+
+
+[Robot,q0] = initializeMicroscope();
+disp('Initialization Completed.');
+
+%% Marker tracking and robot movement
+
+tic; %Start timer
+
+player = vision.DeployableVideoPlayer('Location',[10,100]);
+v = VideoWriter('pivot.avi');
+v.FrameRate = 30;
+open(v)
+
+for k = 1:10:nFramesLeft
+
+%Read Frames
+frameLeft = mov(k).readerLeft ;
+frameRight = mov(k).readerRight;
+
+%Convert to Grayscale
+frameLeftGray = rgb2gray(frameLeft);
+frameRightGray = rgb2gray(frameRight);
+
+%Detect markers in the Left 
+img_left = frameLeftGray > threshold; %Creates binary image
+BW_left = bwareafilt(img_left, 3); %Extract 3 largest blobs
+[centroidLeft,bboxLeft] = step(hblob,BW_left);
+
+%Detect markers in the Right
+img_right = frameRightGray > threshold; %Creates binary image
+BW_right = bwareafilt(img_right, 3); %Extract 3 largest blobs
+[centroidRight,bboxRight] = step(hblob,BW_right);
+
+%Triangulate for all three markers
+point3d_1 = triangulate(centroidLeft(1,:),centroidRight(1,:),stereoParams18);
+point3d_2 = triangulate(centroidLeft(2,:),centroidRight(2,:),stereoParams18);
+point3d_3 = triangulate(centroidLeft(3,:),centroidRight(3,:),stereoParams18);
+
+%Find surgical tip location
+surgicalTip = findSurgicalTip(point3d_1,point3d_2,point3d_3, pivotOffset);
+
+% Insert shape on markers
+rgb = insertShape(frameLeft,'rectangle',bboxLeft(1,:),'Color','black',...
+'LineWidth',3);
+rgb = insertShape(rgb,'rectangle',bboxLeft(2,:),'Color','black',...
+'LineWidth',3);
+rgb = insertShape(rgb,'rectangle',bboxLeft(3,:),'Color','black',...
+'LineWidth',3);
+
+rgb = insertText(rgb,centroidLeft(1,:) - 20,['X: ' num2str(round(point3d_1(1,1)),'%d')...
+' Y: ' num2str(round(point3d_1(1,2)),'%d') ' Z: ' num2str(round(point3d_1(1,3)))],'FontSize',18);
+rgb = insertText(rgb,centroidLeft(2,:) + 50,['X: ' num2str(round(point3d_2(1,1)),'%d')...
+' Y: ' num2str(round(point3d_2(1,2)),'%d') ' Z: ' num2str(round(point3d_2(1,3)))],'FontSize',18);
+rgb = insertText(rgb,centroidLeft(3,:) - 50,['X: ' num2str(round(point3d_3(1,1)),'%d')...
+' Y: ' num2str(round(point3d_3(1,2)),'%d') ' Z: ' num2str(round(point3d_3(1,3)))],'FontSize',18);
+
+player(rgb);
+  
+% World to Microscope Coordinate Mapping
+[xMicroscope, yMicroscope, zMicroscope] = world2Microscope(surgicalTip(1), surgicalTip(2), surgicalTip(3));
+
+% Send Coordinates to AT03 Robot
+q0 = moveMicroscope(xMicroscope, yMicroscope, zMicroscope, q0, Robot);
+
+player(rgb);
+writeVideo(v,rgb); 
+end
+
+release(player)
+close(v);  
+
+toc; %Stop timer
+
+%% Pivot Offset Visualization
+
+figure
+plot3((point3d_3(1,1)), (point3d_3(1,2)), (point3d_3(1,3)), 'ro');
+hold on
+plot3((point3d_2(1,1)), (point3d_2(1,2)), (point3d_2(1,3)), 'bo');
+plot3((point3d_1(1,1)), (point3d_1(1,2)), (point3d_1(1,3)), 'go');
+plot3((surgicalTip(1,1)), (surgicalTip(1,2)), (surgicalTip(1,3)), 'co');
+hold off
---
+++ Accuracy Testing/initializeMicroscope.m
@@ -0,0 +1,65 @@
+function [AT03,q0] = initializeMicroscope()
+%x_frame1,y_frame1,z_frame1
+% % Initialize Toolbox (Robotics Toolbox for MATLAB (release 10.4))
+% startup_rvc;
+% toolboxFile = 'RTB (1).mltbx';
+% installedToolbox = matlab.addons.toolbox.installToolbox(toolboxFile);
+
+% Initialize Robot SerialLink
+L(1)=Link ([0 0 0 pi/2]);
+L(2)=Link ([0 0 118.7 0]);
+L(3)=Link ([0 0 0 pi/2]);
+L(4)=Link ([0 36 0 -pi/2]);
+L(5)=Link ([0 0 0 pi/2]);
+L(6)=Link ([0 5.5 0 0]);
+
+AT03=SerialLink(L,'name','AT03Robot');
+%test ... AT03.model3d = 'CAPSTONE\AT03';
+%start_pos=[0,1.571/2,1.571,-1.571,-1.571/2,0];
+start_pos_new = [0 3.14/2 0 0 -3.14/2 0];
+%AT03.offset=[0 0 0 0 0 0];
+T_start = AT03.fkine(start_pos_new);
+[Start_X,Start_Y,Start_Z] = transl(T_start);
+AT03.plot(start_pos_new);
+
+%x_frame1 = -36; y_frame1= 17; z_frame1=100;
+% Alt_Start_X = x_frame1 ; % -100;
+% Alt_Start_Y = y_frame1; %0;
+% Alt_Start_Z = z_frame1; %112;
+% R = 3.1416; %new pose orientation input
+% P = 0; %new pose orientation input
+% Y = 0; %new pose orientation input
+% T_alt_start = transl(Alt_Start_X,Alt_Start_Y,Alt_Start_Z )*rpy2tr(R,P,Y);
+% q0_alt = AT03.ikine(T_alt_start);%'q0',start_pos_new);
+% AT03.plot(q0_alt);
+% 
+% q0 = q0_alt;
+q0=start_pos_new;
+
+% %test
+% AT03=SerialLink(L,'name','AT03Robot');
+% %AT03.model3d = 'CAPSTONE\AT03';
+% %start_pos=[0,1.571/2,1.571,-1.571,-1.571/2,0];
+% start_pos_new = [0 3.14/2 0 0 -3.14/2 0];
+% %AT03.offset=[0 0 0 0 0 0];
+% T_start = AT03.fkine(start_pos_new);
+% RPY = tr2rpy(T_start);
+% [X,Y,Z] = transl(T_start);
+% 
+% x = 125; %new pose stream in
+% y = -71; %new pose _ stream in
+% z = 65; %new pose _ stream in
+% R = 3.1416;
+% P = 0;
+% Y = 0;
+% % Get q_new to move to new pose
+% T_start = transl(x,y,z) * rpy2tr(R,P,Y);
+% q0 = start_pos_new;
+% qf = AT03.ikine(T_start,'q0',q0);
+ 
+% % Initialize 
+% % start_pos_new = [0 3.14/2 0 0 -3.14/2 0]
+% q0 = qf; %first q0 to give starting pose.
+
+end
+
