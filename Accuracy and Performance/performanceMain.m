25cbc9c10d6565fbf4390edf4e5abb1b596fa2b1
Ginette Hartell <77505600+ghartell@users.noreply.github.com>
2021-03-12 12:20:01 -0500

    Adding equiv FPS rate

--- Accuracy and Performance/performanceMain.m
+++ Accuracy and Performance/performanceMain.m
@@ -139,24 +139,27 @@ close(v);
 %% Output performance metrics
 
 Process_Names = ["Entire Worflow"; "Preprocessing";"Find Tip"; "World2Microscope"; "Control System"];
-Average_Elapsed_Time = zeros(5, 1);
-Stdv_Elapsed_Time = zeros(5, 1);
-Max_Elapsed_Time = zeros(5, 1);
-Min_Elapsed_Time = zeros(5, 1);
+Avg_Time = zeros(5, 1);
+Stdv_Time = zeros(5, 1);
+Max_Time = zeros(5, 1);
+Min_Time = zeros(5, 1);
+Equiv_FPS_Rate = zeros(5, 1);
 
 for i = 1:4
     array = strcat('elapsed_', num2str(i));
-    Average_Elapsed_Time(i+1, 1) = mean(eval(array));
-    Stdv_Elapsed_Time(i+1, 1) = std(eval(array));
-    Max_Elapsed_Time(i+1, 1) = max(eval(array));
-    Min_Elapsed_Time(i+1, 1) = min(eval(array));
+    Avg_Time(i+1, 1) = mean(eval(array));
+    Equiv_FPS_Rate(i+1, 1) = 1/(Avg_Time(i+1, 1));
+    Stdv_Time(i+1, 1) = std(eval(array));
+    Max_Time(i+1, 1) = max(eval(array));
+    Min_Time(i+1, 1) = min(eval(array));
 end
 
-Average_Elapsed_Time(1, 1) = sum(Average_Elapsed_Time(2:5,:));
-Stdv_Elapsed_Time(1, 1) = sqrt(sum((Stdv_Elapsed_Time(2:5,:)).^2));
-Max_Elapsed_Time(1, 1) = sum(Max_Elapsed_Time(2:5,:));
-Min_Elapsed_Time(1, 1) = sum(Min_Elapsed_Time(2:5,:));
+Avg_Time(1, 1) = sum(Avg_Time(2:5,:));
+Equiv_FPS_Rate(1, 1) = 1/(Avg_Time(1, 1));
+Stdv_Time(1, 1) = sqrt(sum((Stdv_Time(2:5,:)).^2));
+Max_Time(1, 1) = sum(Max_Time(2:5,:));
+Min_Time(1, 1) = sum(Min_Time(2:5,:));
 
-T = table(Process_Names, Average_Elapsed_Time, Stdv_Elapsed_Time, Max_Elapsed_Time, Min_Elapsed_Time)
+T = table(Process_Names, Avg_Time, Equiv_FPS_Rate, Stdv_Time, Max_Time, Min_Time)
 
 
