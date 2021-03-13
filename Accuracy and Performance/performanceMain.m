4bb83942c19e5e8b179b2936eeea5630939d3151
Ginette Hartell <77505600+ghartell@users.noreply.github.com>
2021-03-13 12:52:09 -0500

    Moving performance calculations into a separate function

--- Accuracy and Performance/performanceMain.m
+++ Accuracy and Performance/performanceMain.m
@@ -137,33 +137,9 @@ close(v);
 
 %% Output performance metrics
 
-elapsedcut_1 = elapsed_1(5:235);
-elapsedcut_2 = elapsed_2(5:235);
-elapsedcut_3 = elapsed_3(5:235);
-elapsedcut_4 = elapsed_4(5:235);
+[T, Equiv_FPS_Rate] = systemPerformance(elapsed_1,elapsed_2, elapsed_3, elapsed_4);
 
-Process_Names = ["Entire Worflow"; "Preprocessing";"Find Tip"; "World2Microscope"; "Control System"];
-Avg_Time = zeros(5, 1);
-Stdv_Time = zeros(5, 1);
-Max_Time = zeros(5, 1);
-Min_Time = zeros(5, 1);
-
-for i = 1:4
-    array = strcat('elapsedcut_', num2str(i));
-    Avg_Time(i+1, 1) = mean(eval(array));
-    Stdv_Time(i+1, 1) = std(eval(array));
-    Max_Time(i+1, 1) = max(eval(array));
-    Min_Time(i+1, 1) = min(eval(array));
-end
-
-Avg_Time(1, 1) = sum(Avg_Time(2:5,:));
-Equiv_FPS_Rate = 1/(Avg_Time(1, 1));
-Stdv_Time(1, 1) = sqrt(sum((Stdv_Time(2:5,:)).^2));
-Max_Time(1, 1) = sum(Max_Time(2:5,:));
-Min_Time(1, 1) = sum(Min_Time(2:5,:));
-
-T = table(Process_Names, Avg_Time, Stdv_Time, Max_Time, Min_Time)
-
+disp(T);
 fprintf('Equivalent FPS Rate: %3.2f \n', Equiv_FPS_Rate);
 
 %% [Delete Later] Plot the elapsed times
