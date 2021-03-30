7ef4e537930530e70b8ead6c9d26325db2b2f793
Ginette Hartell <77505600+ghartell@users.noreply.github.com>
2021-03-30 16:19:03 -0400

    Removing Phase II Push

--- Phase II Demo/contrib/vlfeat-0.9.20/bin/maci64/objs/svm.d
+++
@@ -1,3 +0,0 @@
-bin/maci64/objs/svm.o bin/maci64/objs/svm.d: vl/svm.c vl/svm.h \
-  vl/generic.h vl/host.h vl/random.h vl/svmdataset.h vl/homkermap.h \
-  vl/mathop.h
--- Phase II Demo/contrib/vlfeat-0.9.20/bin/maci64/objs/svmdataset.d
+++
@@ -1,3 +0,0 @@
-bin/maci64/objs/svmdataset.o bin/maci64/objs/svmdataset.d: \
-  vl/svmdataset.c vl/svmdataset.h vl/generic.h vl/host.h vl/random.h \
-  vl/homkermap.h vl/float.th
